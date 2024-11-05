0f17c937a7f69e046fbc1e4b4b7a1468b68fdbb1
Jonas Klug <jonas.klug@telekom.de>
2024-11-05 09:56:43 +0100

    refactored scalable_errors.go

--- internal/pkg/scalable/scalable_errors.go
+++ internal/pkg/scalable/scalable_errors.go
@@ -5,6 +5,7 @@ import (
 )
 
 // Errors for scalable package
-var errMinReplicasBoundsExceeded = errors.New("error: a HPAs minReplicas can only be set to int32 values larger than 1")
-
-var errNoReplicasSpecified = errors.New("error: workload has no replicas set")
+var (
+	errMinReplicasBoundsExceeded = errors.New("error: a HPAs minReplicas can only be set to int32 values larger than 1")
+	errNoReplicasSpecified       = errors.New("error: workload has no replicas set")
+)
