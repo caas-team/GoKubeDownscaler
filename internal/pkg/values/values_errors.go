0ad5a6ae3b18a1083981effa3f6510c75a5cb82f
Jonas Klug <jonas.klug@telekom.de>
2024-11-16 23:13:58 +0100

    added more errors to values package

--- internal/pkg/values/values_errors.go
+++ internal/pkg/values/values_errors.go
@@ -1,18 +1,52 @@
 package values
 
-import (
-	"errors"
+// ConfigurationErrors
+var (
+	errForceUpAndDownTime = &ConfigurationError{reason: "error: both forceUptime and forceDowntime are defined", caused: ""}
+	errUpAndDownTime      = &ConfigurationError{reason: "error: both uptime and downtime are defined", caused: ""}
+	errTimeAndPeriod      = &ConfigurationError{reason: "error: both a time and a period is defined", caused: ""}
+	errInvalidConfig      = &ConfigurationError{reason: "error: invalid configuration", caused: ""}
 )
 
-// Errors for values package
+type ConfigurationError struct {
+	reason string
+	caused string
+}
+
+func (e *ConfigurationError) Error() string {
+	return e.reason
+}
+
+// ValidationErrors
 var (
-	errForceUpAndDownTime       = errors.New("error: both forceUptime and forceDowntime are defined")
-	errUpAndDownTime            = errors.New("error: both uptime and downtime are defined")
-	errTimeAndPeriod            = errors.New("error: both a time and a period is defined")
-	errInvalidDownscaleReplicas = errors.New("error: downscale replicas value is invalid")
-	errValueNotSet              = errors.New("error: no layer implements this value")
-	errAnnotationNotSet         = errors.New("error: annotation isn't set on workload")
-	errInvalidWeekday           = errors.New("error: specified weekday is invalid")
-	errRelativeTimespanInvalid  = errors.New("error: specified relative timespan is invalid")
-	errTimeOfDayOutOfRange      = errors.New("error: the time of day has fields that are out of rane")
+	errInvalidDownscaleReplicas = &ValidationError{reason: "error: downscale replicas value is invalid", caused: ""}
+	errInvalidWeekday           = &ValidationError{reason: "error: specified weekday is invalid", caused: ""}
+	errRelativeTimespanInvalid  = &ValidationError{reason: "error: specified relative timespan is invalid", caused: ""}
+	errTimeOfDayOutOfRange      = &ValidationError{reason: "error: the time of day has fields that are out of range", caused: ""}
+	errInvalidAnnotation        = &ValidationError{reason: "error: invalid annotation", caused: ""}
 )
+
+type ValidationError struct {
+	reason string
+	caused string
+}
+
+func (e *ValidationError) Error() string {
+	return e.reason
+}
+
+// RuntimeErrors
+var (
+	errValueNotSet      = &RuntimeError{reason: "error: no layer implements this value", caused: ""}
+	errAnnotationNotSet = &RuntimeError{reason: "error: annotation isn't set on workload", caused: ""}
+	errOperationFailed  = &RuntimeError{reason: "error: operation failed", caused: ""}
+)
+
+type RuntimeError struct {
+	reason string
+	caused string
+}
+
+func (e *RuntimeError) Error() string {
+	return e.reason
+}
