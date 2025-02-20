e6ca7376157f6ab001b39b85a6250949c6cdfb31
Samuel Esposito <40698384+samuel-esp@users.noreply.github.com>
2025-02-20 20:54:35 +0100

    refactor: renamed scanAttempt function

--- cmd/kubedownscaler/main.go
+++ cmd/kubedownscaler/main.go
@@ -186,7 +186,7 @@ func startScanning(
 		var waitGroup sync.WaitGroup
 		for _, workload := range workloads {
 			waitGroup.Add(1)
-			scanAttempt(client, ctx, layerCli, layerEnv, config, &waitGroup, workload)
+			attemptScan(client, ctx, layerCli, layerEnv, config, &waitGroup, workload)
 		}
 
 		waitGroup.Wait()
@@ -204,7 +204,7 @@ func startScanning(
 	return nil
 }
 
-func scanAttempt(
+func attemptScan(
 	client kubernetes.Client,
 	ctx context.Context,
 	layerCli, layerEnv *values.Layer,
