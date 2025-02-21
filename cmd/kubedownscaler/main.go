e5bfbff9afa9cdaeff97fda2a06c84795ba98e67
Samuel Esposito <40698384+samuel-esp@users.noreply.github.com>
2025-02-21 19:48:23 +0100

    refactor: attemptScan go func

--- cmd/kubedownscaler/main.go
+++ cmd/kubedownscaler/main.go
@@ -160,7 +160,8 @@ func startScanning(
 		var waitGroup sync.WaitGroup
 		for _, workload := range workloads {
 			waitGroup.Add(1)
-			attemptScan(client, ctx, layerCli, layerEnv, config, &waitGroup, workload)
+
+			go attemptScan(client, ctx, layerCli, layerEnv, config, &waitGroup, workload)
 		}
 
 		waitGroup.Wait()
@@ -186,56 +187,54 @@ func attemptScan(
 	waitGroup *sync.WaitGroup,
 	workload scalable.Workload,
 ) {
-	go func(workload scalable.Workload) {
-		slog.Debug("scanning workload", "workload", workload.GetName(), "namespace", workload.GetNamespace())
+	slog.Debug("scanning workload", "workload", workload.GetName(), "namespace", workload.GetNamespace())
 
-		defer waitGroup.Done()
+	defer waitGroup.Done()
 
-		scanSucceded := false
+	scanSucceded := false
 
-		for retry := 0; retry <= config.MaxRetriesOnConflict; retry++ {
-			err := scanWorkload(workload, client, ctx, layerCli, layerEnv, config)
-			if err != nil {
-				if !(strings.Contains(err.Error(), registry.OptimisticLockErrorMsg)) {
-					slog.Error("failed to scan workload", "error", err, "workload", workload.GetName(), "namespace", workload.GetNamespace())
-					return
-				}
-
-				slog.Warn("workload modified, retrying", "attempt", retry+1, "workload", workload.GetName(), "namespace", workload.GetNamespace())
-
-				updatedWorkload, err := client.GetWorkloads(
-					workload.GetName(),
-					[]string{workload.GetNamespace()},
-					[]string{strings.ToLower(workload.GroupVersionKind().Kind)},
-					ctx,
-				)
-				if err != nil {
-					slog.Error("failed to fetch latest workload", "error", err, "workload", workload.GetName(), "namespace", workload.GetNamespace())
-					return
-				}
-
-				workload = updatedWorkload[0]
-
-				continue
+	for retry := 0; retry <= config.MaxRetriesOnConflict; retry++ {
+		err := scanWorkload(workload, client, ctx, layerCli, layerEnv, config)
+		if err != nil {
+			if !(strings.Contains(err.Error(), registry.OptimisticLockErrorMsg)) {
+				slog.Error("failed to scan workload", "error", err, "workload", workload.GetName(), "namespace", workload.GetNamespace())
+				return
 			}
 
-			slog.Debug("successfully scanned workload", "workload", workload.GetName(), "namespace", workload.GetNamespace())
+			slog.Warn("workload modified, retrying", "attempt", retry+1, "workload", workload.GetName(), "namespace", workload.GetNamespace())
 
-			scanSucceded = true
+			updatedWorkload, err := client.GetWorkloads(
+				workload.GetName(),
+				[]string{workload.GetNamespace()},
+				[]string{strings.ToLower(workload.GroupVersionKind().Kind)},
+				ctx,
+			)
+			if err != nil {
+				slog.Error("failed to fetch latest workload", "error", err, "workload", workload.GetName(), "namespace", workload.GetNamespace())
+				return
+			}
 
-			break
+			workload = updatedWorkload[0]
+
+			continue
 		}
 
-		if !scanSucceded && config.MaxRetriesOnConflict > 0 {
-			slog.Warn("max retries reached, will try again in the next scan", "workload", workload.GetName(), "namespace", workload.GetNamespace())
-			return
-		}
+		slog.Debug("successfully scanned workload", "workload", workload.GetName(), "namespace", workload.GetNamespace())
 
-		if !scanSucceded && config.MaxRetriesOnConflict == 0 {
-			slog.Error("failed to scan workload", "workload", workload.GetName(), "namespace", workload.GetNamespace())
-			return
-		}
-	}(workload)
+		scanSucceded = true
+
+		break
+	}
+
+	if !scanSucceded && config.MaxRetriesOnConflict > 0 {
+		slog.Warn("max retries reached, will try again in the next scan", "workload", workload.GetName(), "namespace", workload.GetNamespace())
+		return
+	}
+
+	if !scanSucceded && config.MaxRetriesOnConflict == 0 {
+		slog.Error("failed to scan workload", "workload", workload.GetName(), "namespace", workload.GetNamespace())
+		return
+	}
 }
 
 // scanWorkload runs a scan on the worklod, determining the scaling and scaling the workload.
