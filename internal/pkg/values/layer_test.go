b68834df18e740bf9b5975ae41b020246a357abe
Jonathan Mayer <jonathan.mayer@telekom.de>
2025-03-07 09:39:48 +0100

    Feat/allow timespans in force up and downtime + fix everything (#132)
    * refactor: relative timespans
    
    * refactor: the rest
    
    * fix: change exclude logic
    
    * revert: refactor out tri state bool
    
    * fix: update LayerID enums and stringer
    
    * feat: allow boolean values in timespans
    
    * fix: exclude workload
    
    * chore: add test
    
    * fix: this might be unintended behaviour free
    
    * chore: bump deps (finally)
    
    * fix: typo
    
    * chore: a to an
    
    Co-authored-by: Jan <157487559+JTaeuber@users.noreply.github.com>
    
    * chore: pr threads
    
    ---------
    
    Co-authored-by: Jan <157487559+JTaeuber@users.noreply.github.com>

--- internal/pkg/values/layer_test.go
+++ internal/pkg/values/layer_test.go
@@ -27,10 +27,26 @@ func TestLayer_checkForIncompatibleFields(t *testing.T) {
 		{
 			name: "forced up and downtime",
 			layer: Layer{
-				ForceUptime:   triStateBool{isSet: true, value: true},
-				ForceDowntime: triStateBool{isSet: true, value: true},
+				ForceUptime:   timeSpans{booleanTimeSpan(true)},
+				ForceDowntime: timeSpans{booleanTimeSpan(true)},
 			},
 			wantErr: true,
+		},
+		{
+			name: "forced up and downtime one false",
+			layer: Layer{
+				ForceUptime:   timeSpans{booleanTimeSpan(false)},
+				ForceDowntime: timeSpans{booleanTimeSpan(true)},
+			},
+			wantErr: true, // this might be changed in the future
+		},
+		{
+			name: "forced up and downtime false",
+			layer: Layer{
+				ForceUptime:   timeSpans{booleanTimeSpan(false)},
+				ForceDowntime: timeSpans{booleanTimeSpan(false)},
+			},
+			wantErr: true, // this might be changed in the future
 		},
 		{
 			name: "downscale replicas invalid",
@@ -105,16 +121,6 @@ func TestLayer_checkForIncompatibleFields(t *testing.T) {
 
 func TestLayer_getCurrentScaling(t *testing.T) {
 	t.Parallel()
-	var (
-		inTimeSpan = timeSpans{absoluteTimeSpan{
-			from: time.Now().Add(-time.Hour),
-			to:   time.Now().Add(time.Hour),
-		}}
-		outOfTimeSpan = timeSpans{absoluteTimeSpan{
-			from: time.Now().Add(-2 * time.Hour),
-			to:   time.Now().Add(-time.Hour),
-		}}
-	)
 
 	tests := []struct {
 		name        string
@@ -124,56 +130,56 @@ func TestLayer_getCurrentScaling(t *testing.T) {
 		{
 			name: "in downtime",
 			layer: Layer{
-				DownTime: inTimeSpan,
+				DownTime: timeSpans{booleanTimeSpan(true)},
 			},
 			wantScaling: ScalingDown,
 		},
 		{
 			name: "out of downtime",
 			layer: Layer{
-				DownTime: outOfTimeSpan,
+				DownTime: timeSpans{booleanTimeSpan(false)},
 			},
 			wantScaling: ScalingUp,
 		},
 		{
 			name: "in uptime",
 			layer: Layer{
-				UpTime: inTimeSpan,
+				UpTime: timeSpans{booleanTimeSpan(true)},
 			},
 			wantScaling: ScalingUp,
 		},
 		{
 			name: "out of uptime",
 			layer: Layer{
-				UpTime: outOfTimeSpan,
+				UpTime: timeSpans{booleanTimeSpan(false)},
 			},
 			wantScaling: ScalingDown,
 		},
 		{
 			name: "in downscaleperiod",
 			layer: Layer{
-				DownscalePeriod: inTimeSpan,
+				DownscalePeriod: timeSpans{booleanTimeSpan(true)},
 			},
 			wantScaling: ScalingDown,
 		},
 		{
 			name: "out of downscaleperiod",
 			layer: Layer{
-				DownscalePeriod: outOfTimeSpan,
+				DownscalePeriod: timeSpans{booleanTimeSpan(false)},
 			},
 			wantScaling: ScalingIgnore,
 		},
 		{
 			name: "in upscaleperiod",
 			layer: Layer{
-				UpscalePeriod: inTimeSpan,
+				UpscalePeriod: timeSpans{booleanTimeSpan(true)},
 			},
 			wantScaling: ScalingUp,
 		},
 		{
 			name: "out of upscaleperiod",
 			layer: Layer{
-				UpscalePeriod: outOfTimeSpan,
+				UpscalePeriod: timeSpans{booleanTimeSpan(false)},
 			},
 			wantScaling: ScalingIgnore,
 		},
@@ -190,6 +196,264 @@ func TestLayer_getCurrentScaling(t *testing.T) {
 
 			scaling := test.layer.getCurrentScaling()
 			assert.Equal(t, test.wantScaling, scaling)
+		})
+	}
+}
+
+func TestLayer_getForceScaling(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name        string
+		layer       Layer
+		wantScaling Scaling
+	}{
+		{
+			name: "forceDowntime",
+			layer: Layer{
+				ForceDowntime: timeSpans{booleanTimeSpan(true)},
+			},
+			wantScaling: ScalingDown,
+		},
+		{
+			name: "forceUptime",
+			layer: Layer{
+				ForceUptime: timeSpans{booleanTimeSpan(true)},
+			},
+			wantScaling: ScalingUp,
+		},
+		{
+			name:        "none set",
+			layer:       Layer{},
+			wantScaling: ScalingNone,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			scaling := test.layer.getForceScaling()
+			assert.Equal(t, test.wantScaling, scaling)
+		})
+	}
+}
+
+func TestLayers_GetCurrentScaling(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name        string
+		layers      Layers
+		wantScaling Scaling
+	}{
+		{
+			name: "ignore false forcing *time",
+			layers: Layers{
+				&Layer{},
+				&Layer{ForceDowntime: timeSpans{booleanTimeSpan(false)}, UpTime: timeSpans{booleanTimeSpan(true)}},
+				&Layer{},
+				&Layer{DownTime: timeSpans{booleanTimeSpan(true)}},
+				&Layer{},
+			},
+			wantScaling: ScalingUp,
+		},
+		{
+			name: "never stops fallthrough",
+			layers: Layers{
+				&Layer{},
+				&Layer{DownTime: timeSpans{booleanTimeSpan(false)}},
+				&Layer{},
+				&Layer{DownTime: timeSpans{booleanTimeSpan(true)}},
+				&Layer{},
+			},
+			wantScaling: ScalingUp,
+		},
+		{
+			name: "force *time never doesn't stop fallthrough",
+			layers: Layers{
+				&Layer{},
+				&Layer{ForceDowntime: timeSpans{booleanTimeSpan(false)}},
+				&Layer{},
+				&Layer{DownTime: timeSpans{booleanTimeSpan(true)}},
+				&Layer{},
+			},
+			wantScaling: ScalingDown,
+		},
+		{
+			name: "none set",
+			layers: Layers{
+				&Layer{},
+				&Layer{},
+				&Layer{},
+				&Layer{},
+				&Layer{},
+			},
+			wantScaling: ScalingNone,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			scaling := test.layers.GetCurrentScaling()
+			assert.Equal(t, test.wantScaling, scaling)
+		})
+	}
+}
+
+func TestLayers_GetExcluded(t *testing.T) {
+	t.Parallel()
+
+	timeUntilTrue := time.Now().AddDate(1, 0, 0)
+	timeUntilFalse := time.Now().AddDate(-1, 0, 0)
+
+	tests := []struct {
+		name   string
+		layers Layers
+		want   bool
+	}{
+		{
+			name: "none set",
+			layers: Layers{
+				&Layer{},
+				&Layer{},
+				&Layer{},
+				&Layer{},
+				&Layer{},
+			},
+			want: false,
+		},
+		{
+			name: "explicit include",
+			layers: Layers{
+				&Layer{},
+				&Layer{},
+				&Layer{Exclude: timeSpans{booleanTimeSpan(true)}},
+				&Layer{},
+				&Layer{Exclude: timeSpans{booleanTimeSpan(false)}},
+			},
+			want: true,
+		},
+		{
+			name: "explicit include",
+			layers: Layers{
+				&Layer{Exclude: timeSpans{booleanTimeSpan(false)}},
+				&Layer{},
+				&Layer{Exclude: timeSpans{booleanTimeSpan(true)}},
+				&Layer{},
+				&Layer{Exclude: timeSpans{booleanTimeSpan(false)}},
+			},
+			want: false,
+		},
+		{
+			name: "exclude until true",
+			layers: Layers{
+				&Layer{ExcludeUntil: &timeUntilTrue},
+				&Layer{},
+				&Layer{},
+				&Layer{},
+				&Layer{},
+			},
+			want: true,
+		},
+		{
+			name: "exclude until false",
+			layers: Layers{
+				&Layer{ExcludeUntil: &timeUntilFalse},
+				&Layer{},
+				&Layer{},
+				&Layer{},
+				&Layer{},
+			},
+			want: false,
+		},
+		{
+			name: "exclude until and exclude same layer false",
+			layers: Layers{
+				&Layer{ExcludeUntil: &timeUntilFalse, Exclude: timeSpans{booleanTimeSpan(false)}},
+				&Layer{},
+				&Layer{},
+				&Layer{},
+				&Layer{},
+			},
+			want: false,
+		},
+		{
+			name: "exclude until and exclude true same layer",
+			layers: Layers{
+				&Layer{ExcludeUntil: &timeUntilFalse, Exclude: timeSpans{booleanTimeSpan(true)}},
+				&Layer{},
+				&Layer{},
+				&Layer{},
+				&Layer{},
+			},
+			want: true,
+		},
+		{
+			name: "exclude until true and exclude same layer",
+			layers: Layers{
+				&Layer{ExcludeUntil: &timeUntilTrue, Exclude: timeSpans{booleanTimeSpan(false)}},
+				&Layer{},
+				&Layer{},
+				&Layer{},
+				&Layer{},
+			},
+			want: true,
+		},
+		{
+			name: "exclude until and exclude true different layers",
+			layers: Layers{
+				&Layer{ExcludeUntil: &timeUntilFalse},
+				&Layer{},
+				&Layer{Exclude: timeSpans{booleanTimeSpan(true)}},
+				&Layer{},
+				&Layer{},
+			},
+			want: true,
+		},
+		{
+			name: "exclude until true and exclude different layers",
+			layers: Layers{
+				&Layer{ExcludeUntil: &timeUntilTrue},
+				&Layer{},
+				&Layer{Exclude: timeSpans{booleanTimeSpan(false)}},
+				&Layer{},
+				&Layer{},
+			},
+			want: true,
+		},
+		{
+			name: "exclude and exclude until different layers",
+			layers: Layers{
+				&Layer{},
+				&Layer{},
+				&Layer{Exclude: timeSpans{booleanTimeSpan(true)}},
+				&Layer{ExcludeUntil: &timeUntilFalse},
+				&Layer{},
+			},
+			want: true,
+		},
+		{
+			name: "exclude and exclude until different layers",
+			layers: Layers{
+				&Layer{},
+				&Layer{Exclude: timeSpans{booleanTimeSpan(false)}},
+				&Layer{},
+				&Layer{ExcludeUntil: &timeUntilTrue},
+				&Layer{},
+			},
+			want: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			got := test.layers.GetExcluded()
+			assert.Equal(t, test.want, got)
 		})
 	}
 }
