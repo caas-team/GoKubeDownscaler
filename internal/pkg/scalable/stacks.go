3bfb75753590388764ffe77121778dbfcecb2699
Samuel Esposito <40698384+samuel-esp@users.noreply.github.com>
2025-03-11 11:33:39 +0100

    Update internal/pkg/scalable/stacks.go
    Co-authored-by: Jonathan Mayer <jonathanmayer2007@gmail.com>

--- internal/pkg/scalable/stacks.go
+++ internal/pkg/scalable/stacks.go
@@ -9,8 +9,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// getStacks is the getResourceFunc for Zalando Stacks. //nolint:dupl.
-//
+// getStacks is the getResourceFunc for Zalando Stacks.
 //nolint:dupl // necessary to handle different workload types separately
 func getStacks(namespace string, clientsets *Clientsets, ctx context.Context) ([]Workload, error) {
 	stacks, err := clientsets.Zalando.ZalandoV1().Stacks(namespace).List(ctx, metav1.ListOptions{})
