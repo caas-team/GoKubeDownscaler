f9e517c856b845e9a6b52a50441d5f2abde8edd7
Samuel Esposito <40698384+samuel-esp@users.noreply.github.com>
2025-03-11 15:42:25 +0100

    refactor: removed nolint

--- internal/pkg/scalable/stacks.go
+++ internal/pkg/scalable/stacks.go
@@ -10,8 +10,6 @@ import (
 )
 
 // getStacks is the getResourceFunc for Zalando Stacks.
-//
-//nolint:dupl // necessary to handle different workload types separately
 func getStacks(namespace string, clientsets *Clientsets, ctx context.Context) ([]Workload, error) {
 	stacks, err := clientsets.Zalando.ZalandoV1().Stacks(namespace).List(ctx, metav1.ListOptions{})
 	if err != nil {
