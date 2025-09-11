86eca010190835086935f56d451253a373c33849
Samuel Esposito <40698384+samuel-esp@users.noreply.github.com>
2025-09-11 19:46:57 +0200

    refactor: log message when secret is being created

--- internal/pkg/scalable/cronjobs.go
+++ internal/pkg/scalable/cronjobs.go
@@ -33,7 +33,7 @@ func getCronJobs(namespace string, clientsets *Clientsets, ctx context.Context) 
 
 // parseCronJobFromAdmissionRequest parses the admission review and returns the cronjob wrapped in a Workload.
 //
-//nolint:ireturn // this function should return an interface type
+// nolint: ireturn // this function should return an interface type
 func parseCronJobFromAdmissionRequest(rawObject []byte) (Workload, error) {
 	var cj batch.CronJob
 	if err := json.Unmarshal(rawObject, &cj); err != nil {
@@ -122,7 +122,7 @@ func (c *cronJob) Update(clientsets *Clientsets, ctx context.Context) error {
 
 // Copy creates a deep copy of the given Workload, which is expected to be a suspendScaledWorkload wrapping a cronJob.
 //
-//nolint:ireturn // this function should return an interface type
+// nolint: ireturn // this function should return an interface type
 func (c *cronJob) Copy() (Workload, error) {
 	if c.CronJob == nil {
 		return nil, newNilUnderlyingObjectError(CronJobKind)
