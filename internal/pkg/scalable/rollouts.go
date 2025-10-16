038631878a3a674346297a66eda5f6d088923968
Samuel Esposito <40698384+samuel-esp@users.noreply.github.com>
2025-10-16 22:14:12 +0200

    refactor: removing unused import from rollout class

--- internal/pkg/scalable/rollouts.go
+++ internal/pkg/scalable/rollouts.go
@@ -6,8 +6,6 @@ import (
 	"encoding/json"
 	"fmt"
 
-	admissionv1 "k8s.io/api/admission/v1"
-
 	argov1alpha1 "github.com/argoproj/argo-rollouts/pkg/apis/rollouts/v1alpha1"
 	"github.com/caas-team/gokubedownscaler/internal/pkg/metrics"
 	"github.com/caas-team/gokubedownscaler/internal/pkg/values"
